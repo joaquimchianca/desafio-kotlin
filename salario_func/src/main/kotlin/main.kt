f9eeab54823e3799fce17b73c69a499b06f0c67f
joaquimchianca <joaquimchianca@gmail.com>
2023-08-22 14:49:03 -0300

    feat: resolvido calculo dos funcionarios

--- salario_func/src/main/kotlin/main.kt
+++ salario_func/src/main/kotlin/main.kt
@@ -1,3 +1,20 @@
 fun main() {
+    val salario = readlnOrNull()?.toFloat()
+    val adicional = readlnOrNull()?.toFloat()
 
+    if (salario == null || adicional == null || salario < 0 || adicional < 0) {
+        println("salario ou adicional invalidos")
+        return
+    }
+
+    val aliquota = if (salario > 0 && salario <= 1100) {
+        0.05F
+    } else if (salario > 1100 && salario <= 2500) {
+        0.1F
+    } else {
+        0.15F
+    }
+
+    val imposto = salario * aliquota
+    println((salario - imposto) + adicional)
 }
